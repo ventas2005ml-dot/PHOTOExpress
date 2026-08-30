a9cd689abbe474d3d94feb8838a4530e31d108e0
PhotoExpress <ventas2005ml@gmail.com>
2026-08-29 22:41:43 -0300

    feat: conexion a PostgreSQL configurada

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const db = require('./config/database')
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-// Rutas
 app.get('/', (req, res) => {
   res.json({ mensaje: 'PHOTOExpress API funcionando', version: '1.0.0' })
 })
